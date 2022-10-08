e04a9309b5e2921e960164f5e88ece0f9d6141e3
Andrea Prina <and.prina@gmail.com>
2022-10-08 17:58:51 +0200

    Install Bootstrap and create main vue component

---
+++ client/src/components/MainComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center p-3">
+                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 d-flex justify-content-center">
+                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center">
+                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
+                </div>
+                <div class="col-4 d-flex justify-content-center">
+                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    
+    name : "MainComponent",
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .month-card {
+
+        border: 1px solid red;
+        border-radius: 5px;
+        width: 25vw;
+        height: 25vw;
+
+    }
+
+</style>
