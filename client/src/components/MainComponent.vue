7a015cde170e6fef9aa62831f546b9d53f9c28c7
Andrea Prina <and.prina@gmail.com>
2022-10-08 22:17:18 +0200

    Add get fruits API call from vue

--- client/src/components/MainComponent.vue
+++ client/src/components/MainComponent.vue
@@ -1,71 +1,49 @@
 <template>
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center p-3">
-                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-4 d-flex justify-content-center">
-                    <div class="month-card d-flex align-items-center justify-content-center">JAN</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center">
-                    <div class="month-card d-flex align-items-center justify-content-center">FEB</div>
-                </div>
-                <div class="col-4 d-flex justify-content-center">
-                    <div class="month-card d-flex align-items-center justify-content-center">MAR</div>
-                </div>
-            </div>
-        </div>
+        <input type="text" name="" id="" v-model="month" placeholder="">
+        <button @click="getFreshFruits">PRESS ME</button>
+        <ul>
+            <li v-for="fruit in freshFruit" :key="fruit.id">
+                {{ fruit[0] }}
+            </li>
+        </ul>
     </main>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     
     name : "MainComponent",
 
+    data : function(){
+        return {
+
+            month : "",
+            freshFruit : [],
+        }
+    },
+
+    methods : {
+
+        getFreshFruits : function(){
+            axios.get(`http://127.0.0.1:5000/fruit?month=${this.month}`)
+            .then((result) => {
+                this.freshFruit = result.data.data.fruits
+                console.log(this.freshFruit);
+            })
+            .catch((err) => {
+                console.warn(err);
+            })
+        },
+
+    }
+
 }
+
 </script>
 
 <style lang="scss" scoped>
-
-    .month-card {
-
-        border: 1px solid red;
-        border-radius: 5px;
-        width: 25vw;
-        height: 25vw;
-
-    }
 
 </style>
