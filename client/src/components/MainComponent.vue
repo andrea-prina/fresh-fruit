85c36d839f47464c545a0b7ecd7eb2e7a8854ca3
Andrea Prina <and.prina@gmail.com>
2022-10-08 23:33:11 +0200

    Format months buttons

--- client/src/components/MainComponent.vue
+++ client/src/components/MainComponent.vue
@@ -1,10 +1,8 @@
 <template>
     <main>
         <div class="container">
-            <div class="row">
-                <div class="col-2" v-for="month in months" :key="month[0]">
-                    <button @click="getFreshFruits(month[0])">{{ month[2] }}</button>
-                </div>
+            <div class="row d-flex justify-content-around">
+                <button class="month-btn"  @click="getFreshFruits(month[0])" v-for="month in months" :key="month[0]">{{ month[2] }}</button>
             </div>
         </div>
         <ul>
@@ -64,5 +62,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+    .month-btn {
+        width: calc((100vw - 1rem * 2 * 3) / 3);
+        height: calc((100vw - 1rem * 2 * 3) / 3);
+        margin: 1rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #FFF;
+    }
 
 </style>
