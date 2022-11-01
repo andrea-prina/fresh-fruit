cd5fabff3d97fb5b504cf5dabebffd68fcb93ca8
Andrea Prina <and.prina@gmail.com>
2022-11-01 15:12:01 +0100

    Change btns column style using Bootstrap

--- client/src/components/MainComponent.vue
+++ client/src/components/MainComponent.vue
@@ -1,8 +1,10 @@
 <template>
     <main>
         <div class="container">
-            <div class="row d-flex justify-content-around">
-                <button class="month-btn"  @click="getFreshFruits(month[0])" v-for="month in months" :key="month[0]">{{ month[2] }}</button>
+            <div class="row g-2 mb-2">
+                <div class="col-4 col-md-2 col-lg-1" v-for="month in months" :key="month[0]">
+                    <button class="month-btn" @click="getFreshFruits(month[0])">{{ month[2] }}</button>
+                </div>
             </div>
         </div>
         <ul>
@@ -64,13 +66,7 @@ export default {
 <style lang="scss" scoped>
 
     .month-btn {
-        width: calc((100vw - 1rem * 2 * 3) / 3);
-        height: calc((100vw - 1rem * 2 * 3) / 3);
-        margin: 1rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: #FFF;
+        width: 100%;
     }
 
 </style>
